15ffe7c1f2f1da9aedfcbd715cef9be48b91be64
Zdravko Galchov <zdravko.galchov@gmail.com>
2024-10-31 15:20:12 +0000

    Added more solutions

--- 03_data_operations_with_queries/exercise_project/caller.py
+++ 03_data_operations_with_queries/exercise_project/caller.py
@@ -13,6 +13,7 @@ from main_app.models import (
     Location,
     Car,
     Task,
+    HotelRoom,
 )
 
 
@@ -133,7 +134,7 @@ def complete_odd_tasks() -> None:
     Task.objects.bulk_update(tasks, ['is_finished'])
 
 
-def encode_and_replace(text: str, task_title: str):
+def encode_and_replace(text: str, task_title: str) -> None:
     # tasks = Task.objects.filter(title=task_title)
     #
     # new_description = ''
@@ -148,6 +149,45 @@ def encode_and_replace(text: str, task_title: str):
     # Optimized solution:
     decoded_text = ''.join(chr(ord(symbol) - 3) for symbol in text)
     Task.objects.filter(title=task_title).update(description=decoded_text)
+
+
+def get_deluxe_rooms() -> str:
+    deluxe_rooms = HotelRoom.objects.filter(room_type="Deluxe")
+    even_deluxe_rooms = [str(r) for r in deluxe_rooms if r.id % 2 == 0]
+
+    return "\n".join(even_deluxe_rooms)
+
+
+def increase_room_capacity() -> None:
+    rooms = HotelRoom.objects.all().order_by('id')
+
+    previous_room_capacity = None
+
+    for room in rooms:
+        if not room.is_reserved:
+            continue
+
+        if previous_room_capacity is not None:
+            room.capacity += previous_room_capacity
+        else:
+            room.capacity += room.id
+
+        previous_room_capacity = room.capacity
+
+    HotelRoom.objects.bulk_update(rooms, ['capacity'])
+
+
+def reserve_first_room() -> None:
+    room = HotelRoom.objects.first()
+    room.is_reserved = True
+    room.save()
+
+
+def delete_last_room() -> None:
+    room = HotelRoom.objects.last()
+
+    if not room.is_reserved:
+        room.delete()
 
 
 # print(create_pet('Buddy', 'Dog'))
@@ -183,3 +223,7 @@ def encode_and_replace(text: str, task_title: str):
 
 # encode_and_replace("Zdvk#wkh#glvkhv$", "Simple Task")
 # print(Task.objects.get(title='Simple Task').description)
+
+# print(get_deluxe_rooms())
+# reserve_first_room()
+# print(HotelRoom.objects.get(room_number=401).is_reserved)
