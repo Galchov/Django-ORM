584eefd0a9b2f13f2991dc4bca7c2d9366cdf735
Zdravko Galchov <zdravko.galchov@gmail.com>
2024-11-16 16:31:11 +0000

    Started the exercise

--- 04_working_with_queries/exercise_project/caller.py
+++ 04_working_with_queries/exercise_project/caller.py
@@ -1,9 +1,125 @@
 import os
-
+from typing import List
 
 import django
+from django.db.models import Case, When, Value
 
+from main_app.choices import OperationSystemChoice
 
 # Set up Django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
+
+
+from main_app.models import (
+    ArtworkGallery,
+    Laptop,
+)
+
+
+# --- Exercise 1 ---
+def show_highest_rated_art() -> str:
+    best_artwork = ArtworkGallery.objects.order_by('-rating', 'id').first()
+
+    return f"{best_artwork.art_name} is the highest-rated art with a {best_artwork.rating} rating!"
+
+
+def bulk_create_arts(first_art: ArtworkGallery, second_art: ArtworkGallery) -> None:
+    arts_list = [first_art, second_art]
+
+    ArtworkGallery.objects.bulk_create(arts_list)
+
+
+def delete_negative_rated_arts() -> None:
+    ArtworkGallery.objects.filter(rating__lt=0).delete()
+
+
+# --- Exercise 2 ---
+def show_the_most_expensive_laptop() -> str:
+    laptop = Laptop.objects.order_by('-price', '-id').first()
+
+    return f"{laptop.brand} is the most expensive laptop available for {laptop.price}$!"
+
+
+def bulk_create_laptops(args: List[Laptop]) -> None:
+    Laptop.objects.bulk_create(args)
+
+
+def update_to_512_GB_storage() -> None:
+    Laptop.objects.filter(brand__in=('Asus', 'Lenovo')).update(storage=512)
+
+
+def update_to_16_GB_memory() -> None:
+    Laptop.objects.filter(brand__in=('Apple', 'Dell', 'Acer')).update(memory=16)
+
+
+def update_operation_systems() -> None:
+    # Laptop.objects.filter(brand='Asus').update(operation_system='Windows')
+    # Laptop.objects.filter(brand='Apple').update(operation_system='MacOS')
+    # Laptop.objects.filter(brand__in=('Dell', 'Acer')).update(operation_system='Linux')
+    # Laptop.objects.filter(brand='Lenovo').update(operation_system='Chrome OS')
+
+    # More optimal solution
+    Laptop.objects.update(
+        operation_system=Case(
+            When(brand='Asus', then=Value(OperationSystemChoice.WINDOWS)),
+            When(brand='Apple', then=Value(OperationSystemChoice.MACOS)),
+            When(brand__in=('Dell', 'Acer'), then=Value(OperationSystemChoice.LINUX)),
+            When(brand='Lenovo', then=Value(OperationSystemChoice.CHROME_OS)),
+        )
+    )
+
+
+def delete_inexpensive_laptops() -> None:
+    Laptop.objects.filter(price__lt=1200).delete()
+
+
+# artwork1 = ArtworkGallery(artist_name='Vincent van Gogh', art_name='Starry Night', rating=4, price=1200000.0)
+# artwork2 = ArtworkGallery(artist_name='Leonardo da Vinci', art_name='Mona Lisa', rating=5, price=1500000.0)
+#
+# # Bulk saves the instances
+# bulk_create_arts(artwork1, artwork2)
+# print(show_highest_rated_art())
+# print(ArtworkGallery.objects.all())
+
+
+# laptop1 = Laptop(
+#     brand='Asus',
+#     processor='Intel Core i5',
+#     memory=8,
+#     storage=256,
+#     operation_system='MacOS',
+#     price=899.99
+# )
+# laptop2 = Laptop(
+#     brand='Apple',
+#     processor='Chrome OS',
+#     memory=16,
+#     storage=256,
+#     operation_system='MacOS',
+#     price=1399.99
+# )
+# laptop3 = Laptop(
+#     brand='Lenovo',
+#     processor='AMD Ryzen 7',
+#     memory=12,
+#     storage=256,
+#     operation_system='Linux',
+#     price=999.99,
+# )
+#
+# # Create a list of instances
+# laptops_to_create = [laptop1, laptop2, laptop3]
+#
+# # Use bulk_create to save the instances
+# bulk_create_laptops(laptops_to_create)
+#
+# update_to_512_GB_storage()
+# update_operation_systems()
+#
+# # Retrieve 2 laptops from the database
+# asus_laptop = Laptop.objects.filter(brand__exact='Asus').get()
+# lenovo_laptop = Laptop.objects.filter(brand__exact='Lenovo').get()
+#
+# print(asus_laptop.storage)
+# print(lenovo_laptop.operation_system)
