5e7cf483b95bd916ba7b74bf7ea2ebac6246a04c
Zdravko <zdravko.galchov@gmail.com>
2025-04-25 17:19:41 +0100

    Added 'Migrations and Django Admin' lab project

--- django_project/orm_skeleton/settings.py
+++ django_project/orm_skeleton/settings.py
@@ -75,6 +75,7 @@ WSGI_APPLICATION = 'orm_skeleton.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
@@ -85,6 +86,7 @@ DATABASES = {
         "PORT": "5432",
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
