05e7ef7d057611d9acad5fee1fbf684e5367702b
Zdravko <zdravko.galchov@gmail.com>
2025-04-29 20:37:13 +0100

    Added a new workshop project

--- django_project/caller.py
+++ django_project/caller.py
@@ -1,10 +1,10 @@
 import os
 import django
+from datetime import date
 
 # Set up Django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
 # Import your models here
-
-# Create queries within functions
+# Run and print your queries
--- django_project/orm_skeleton/settings.py
+++ django_project/orm_skeleton/settings.py
@@ -75,17 +75,24 @@ WSGI_APPLICATION = 'orm_skeleton.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "django_orm_3",
-        "USER": "postgres",
-        "PASSWORD": "wagnergroup",
-        "HOST": "localhost",
-        "PORT": "5432",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "",
+#         "USER": "",
+#         "PASSWORD": "",
+#         "HOST": "localhost",
+#         "PORT": "5432",
+#     }
+# }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
