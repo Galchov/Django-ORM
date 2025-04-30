94ab60bd57e70c27cffd3885fff81b2326f4f0d8
Zdravko <zdravko.galchov@gmail.com>
2025-04-30 14:46:26 +0100

    Added a new project for exercise

--- django_project/orm_skeleton/settings.py
+++ django_project/orm_skeleton/settings.py
@@ -79,14 +79,34 @@ WSGI_APPLICATION = 'orm_skeleton.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "",
-        "USER": "",
-        "PASSWORD": "",
+        "NAME": "orm_4_exercise",
+        "USER": "postgres",
+        "PASSWORD": "wagnergroup",
         "HOST": "localhost",
         "PORT": "5432",
     }
 }
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'DEBUG',
+    },
+    'loggers': {
+        'django.db.backends': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+    },
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
