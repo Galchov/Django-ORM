4504096c34e5c7a642549a0134e4a947c2bcaa51
Zdravko <zdravko.galchov@gmail.com>
2025-04-30 15:17:13 +0100

    Added some examples

--- django_project/caller.py
+++ django_project/caller.py
@@ -5,6 +5,48 @@ import django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
-# Import your models here
 
-# Create queries within functions
+from main_app.models import Pet, Artifact
+
+
+def create_pet(name: str, species: str) -> str:
+    pet = Pet.objects.create(
+        name=name,
+        species=species,
+    )
+
+    return f"{name} is a very cute {species}!"
+
+
+def create_artifact(name: str, origin: str, age: int, description: str, is_magical: bool) -> str:
+    artifact = Artifact.objects.create(
+        name=name,
+        origin=origin,
+        age=age,
+        description=description,
+        is_magical=is_magical,
+    )
+
+    return f"The artifact {name} is {age} years old!"
+
+
+def rename_artifact(artifact: Artifact, new_name: str) -> None:
+    if artifact.is_magical and artifact.age > 250:
+        artifact.name = new_name
+        artifact.save()
+
+
+def delete_all_artifacts() -> None:
+    Artifact.objects.all().delete()
+
+
+# Task 1:
+# print(create_pet('Buddy', 'Dog'))
+# print(create_pet('Whiskers', 'Cat'))
+# print(create_pet('Rocky', 'Hamster'))
+
+# Task 2:
+# print(create_artifact('Ancient Sword', 'Lost Kingdom', 500, 'A legendary sword with a rich history', True))
+# artifact_object = Artifact.objects.get(name='Ancient Sword')
+# rename_artifact(artifact_object, 'Ancient Shield')
+# print(artifact_object.name)
