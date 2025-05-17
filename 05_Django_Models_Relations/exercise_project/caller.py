8a1e2cb748a8a5d7be327244ced9369372b8cb2e
Zdravko <zdravko.galchov@gmail.com>
2025-05-17 17:32:12 +0100

    Added examples

--- 05_Django_Models_Relations/exercise_project/caller.py
+++ 05_Django_Models_Relations/exercise_project/caller.py
@@ -5,6 +5,25 @@ import django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
-# Import your models here
 
-# Create queries within functions
+from main_app.models import Author, Book
+
+
+def show_all_authors_with_their_books() -> str:
+    authors = Author.objects.all().order_by('pk')
+    authors_with_books = []
+
+    for author in authors:
+        author_books = author.books.all()
+
+        if not author_books:
+            continue
+
+        books_titles = ', '.join(book.title for book in author_books)
+        authors_with_books.append(f"{author.name} has written - {books_titles}!")
+    
+    return '\n'.join(authors_with_books)
+
+
+def delete_all_authors_without_books() -> None:
+    Author.objects.filter(books__isnull=True).delete()
--- 05_Django_Models_Relations/exercise_project/main_app/models.py
+++ 05_Django_Models_Relations/exercise_project/main_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=40)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    author = models.ForeignKey(to=Author, on_delete=models.CASCADE, related_name='books')
+
+    def __str__(self):
+        return self.title
