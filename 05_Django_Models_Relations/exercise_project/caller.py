db01ae470998ecea1a7bbfaa7235abd759c77005
Zdravko <zdravko.galchov@gmail.com>
2025-05-18 17:38:07 +0100

    Added more examples

--- 05_Django_Models_Relations/exercise_project/caller.py
+++ 05_Django_Models_Relations/exercise_project/caller.py
@@ -6,7 +6,8 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
 
-from main_app.models import Author, Book
+from django.db.models import QuerySet
+from main_app.models import Author, Book, Song, Artist, Product, Review
 
 
 def show_all_authors_with_their_books() -> str:
@@ -27,3 +28,48 @@ def show_all_authors_with_their_books() -> str:
 
 def delete_all_authors_without_books() -> None:
     Author.objects.filter(books__isnull=True).delete()
+
+
+def add_song_to_artist(artist_name: str, song_title: str) -> None:
+    artist = Artist.objects.get(name=artist_name)
+    song = Song.objects.get(title=song_title)
+    artist.songs.add(song)
+
+
+def get_songs_by_artist(artist_name: str) -> QuerySet:
+    artist = Artist.objects.get(name=artist_name)
+    songs = artist.songs.all().order_by('-pk')
+    return songs
+
+
+def remove_song_from_artist(artist_name: str, song_title: str) -> None:
+    artist = Artist.objects.get(name=artist_name)
+    song = Song.objects.get(title=song_title)
+    artist.songs.remove(song)
+
+
+def calculate_average_rating_for_product_by_name(product_name: str) -> float:
+    product = Product.objects.get(name=product_name)
+    product_reviews = product.reviews.all()
+
+    total_rating = 0
+    for review in product_reviews:
+        review_rating = review.rating
+        total_rating += review_rating
+
+    return total_rating / product_reviews.count()
+
+
+def get_reviews_with_high_ratings(threshold: int) -> QuerySet:
+    products = Product.objects.all()
+    high_ratings_reviews = []
+
+    for product in products:
+        reviews = product.reviews.all()
+        for review in reviews:
+            if review.rating >= threshold:
+                high_ratings_reviews.append(review)
+    
+    return high_ratings_reviews
+
+# TODO: Continue with the Shop functions
--- 05_Django_Models_Relations/exercise_project/main_app/models.py
+++ 05_Django_Models_Relations/exercise_project/main_app/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 
+# Exercise 1: Library
+
 class Author(models.Model):
     name = models.CharField(max_length=40)
 
@@ -15,3 +17,32 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+
+
+# Exercise 2: Music App
+
+class Song(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+
+
+class Artist(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    songs = models.ManyToManyField(Song, related_name='artists')
+
+
+# Exercise 3: Shop
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Review(models.Model):
+    description = models.TextField(max_length=200)
+    rating = models.PositiveSmallIntegerField()
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='reviews')
+
+    def __str__(self):
+        return f"{self.rating} => {self.description}"
