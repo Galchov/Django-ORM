3cd410b74e141d5ec3c04a438a5bd7b39043c15f
Zdravko <zdravko.galchov@gmail.com>
2025-05-26 18:45:00 +0100

    feat: add reviews option for the menu

--- 07_advanced_django_model_techniques/lab_project/main_app/models.py
+++ 07_advanced_django_model_techniques/lab_project/main_app/models.py
@@ -79,3 +79,30 @@ class FoodCriticRestaurantReview(RestaurantReview):
     class Meta(RestaurantReview.Meta):
         verbose_name = "Food Critic Review"
         verbose_name_plural = "Food Critic Reviews"
+
+
+class MenuReview(models.Model):
+    reviewer_name = models.CharField(max_length=100)
+    menu = models.ForeignKey(
+        to=Menu,
+        on_delete=models.CASCADE,
+    )
+    review_content = models.TextField()
+    rating = models.PositiveIntegerField(
+        validators=[
+            MaxValueValidator(5, message=None),
+        ]
+    )
+
+    class Meta:
+        ordering = ['-rating']
+        verbose_name = 'Menu Review'
+        verbose_name_plural = 'Menu Reviews'
+        unique_together = ['reviewer_name', 'menu']
+        indexes = [
+            models.Index(
+                fields=['menu'],
+                name='main_app_menu_review_menu_id'
+            )
+        ]
+        
