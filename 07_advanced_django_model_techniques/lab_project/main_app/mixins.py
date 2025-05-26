668659dd15d81c5fa35a723ab8fab9f48895ee00
Zdravko <zdravko.galchov@gmail.com>
2025-05-26 19:21:11 +0100

    refactor: add mixin to improve reviews models' maintainability

---
+++ 07_advanced_django_model_techniques/lab_project/main_app/mixins.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.core.validators import MaxValueValidator
+
+
+class ReviewMixin(models.Model):
+    review_content = models.TextField()
+    rating = models.PositiveIntegerField(
+        validators=[
+            MaxValueValidator(5, message=None),
+        ],
+    )
+
+    class Meta:
+        abstract = True
+        ordering = ['-rating']
+        
--- 07_advanced_django_model_techniques/lab_project/main_app/models.py
+++ 07_advanced_django_model_techniques/lab_project/main_app/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.core.validators import MinLengthValidator, MaxLengthValidator, MinValueValidator, MaxValueValidator
 from main_app.validators import validate_menu_categories
+from main_app.mixins import ReviewMixin
 
 
 class Restaurant(models.Model):
@@ -45,22 +46,15 @@ class Menu(models.Model):
     )
 
 
-class RestaurantReview(models.Model):
+class RestaurantReview(ReviewMixin):
     reviewer_name = models.CharField(max_length=100)
     restaurant = models.ForeignKey(
         to=Restaurant,
         on_delete=models.CASCADE,
     )
-    review_content = models.TextField()
-    rating = models.PositiveIntegerField(
-        validators=[
-            MaxValueValidator(5, message=None),
-        ],
-    )
 
-    class Meta:
+    class Meta(ReviewMixin.Meta):
         abstract = True
-        ordering = ['-rating']
         verbose_name = "Restaurant Review"
         verbose_name_plural = "Restaurant Reviews"
         unique_together = ['reviewer_name', 'restaurant']
@@ -81,21 +75,14 @@ class FoodCriticRestaurantReview(RestaurantReview):
         verbose_name_plural = "Food Critic Reviews"
 
 
-class MenuReview(models.Model):
+class MenuReview(ReviewMixin):
     reviewer_name = models.CharField(max_length=100)
     menu = models.ForeignKey(
         to=Menu,
         on_delete=models.CASCADE,
     )
-    review_content = models.TextField()
-    rating = models.PositiveIntegerField(
-        validators=[
-            MaxValueValidator(5, message=None),
-        ]
-    )
 
-    class Meta:
-        ordering = ['-rating']
+    class Meta(ReviewMixin.Meta):
         verbose_name = 'Menu Review'
         verbose_name_plural = 'Menu Reviews'
         unique_together = ['reviewer_name', 'menu']
@@ -105,4 +92,3 @@ class MenuReview(models.Model):
                 name='main_app_menu_review_menu_id'
             )
         ]
-        
