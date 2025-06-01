c96f064f04bc16fbd5326c50a2a7f68adbdeb673
Zdravko <zdravko.galchov@gmail.com>
2025-06-01 15:48:55 +0100

    feat: add custom manager for 'RealEstateListing' model

--- 08_advanced_queries_in_django/exercise_project/main_app/models.py
+++ 08_advanced_queries_in_django/exercise_project/main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from main_app.managers import RealEstateListingManager
 
 
 class RealEstateListing(models.Model):
@@ -13,6 +14,8 @@ class RealEstateListing(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     bedrooms = models.PositiveIntegerField()
     location = models.CharField(max_length=100)
+
+    objects = RealEstateListingManager()
 
 
 class VideoGame(models.Model):
