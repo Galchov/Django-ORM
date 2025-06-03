f3a414e899eb1ee92efb8084ffae0edab888a9ae
Zdravko <zdravko.galchov@gmail.com>
2025-06-03 15:00:03 +0100

    feat: add class methods to Exercise model

--- 08_advanced_queries_in_django/exercise_project/caller.py
+++ 08_advanced_queries_in_django/exercise_project/caller.py
@@ -7,7 +7,7 @@ django.setup()
 
 
 from datetime import date
-from main_app.models import RealEstateListing, VideoGame, BillingInfo, Invoice, Technology, Project, Programmer, Task
+from main_app.models import RealEstateListing, VideoGame, BillingInfo, Invoice, Technology, Project, Programmer, Task, Exercise
 
 
 ##### Test Exercise 1 #####
@@ -122,3 +122,27 @@ from main_app.models import RealEstateListing, VideoGame, BillingInfo, Invoice, 
 # print("Recent Completed Tasks:")
 # for task in recent_completed:
 #     print('- ' + task.title)
+
+
+##### Test Exercise 6 #####
+
+# # Print the results
+# long_and_hard_exercises = Exercise.get_long_and_hard_exercises()
+# print("Long and hard exercises:")
+# for exercise in long_and_hard_exercises:
+#     print('- ' + exercise.name)
+
+# short_and_easy_exercises = Exercise.get_short_and_easy_exercises()
+# print("Short and easy exercises:")
+# for exercise in short_and_easy_exercises:
+#     print('- ' + exercise.name)
+
+# exercises_within_duration = Exercise.get_exercises_within_duration(20, 40)
+# print(f"Exercises within 20 - 40 minutes:")
+# for exercise in exercises_within_duration:
+#     print('- ' + exercise.name)
+
+# exercises_with_difficulty_and_repetitions = Exercise.get_exercises_with_difficulty_and_repetitions(6, 15)
+# print(f"Exercises with difficulty 6+ and repetitions 15+:")
+# for exercise in exercises_with_difficulty_and_repetitions:
+#     print('- ' + exercise.name)
